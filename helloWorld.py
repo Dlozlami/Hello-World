8bd49ccb168dbef600d723e45c195bd34a47e07b
Dlozi <dlozi.mthethwa@gmail.com>
2024-01-27 12:59:16 +0200

    feat: add requirements.txt

---
+++ helloWorld.py
@@ -0,0 +1,10 @@
+import messages
+
+greeting = "Hello World!";
+print(greeting);
+
+#Lets get more complex
+name = input("What is your name? ")
+print("Hi, "+name)
+
+messages.goodAfternoon()
---
+++ messages.py
@@ -0,0 +1,5 @@
+def goodMorning():
+    print("Good morning, what a lovely day.")
+
+def goodAfternoon():
+    print("Good afternoon, care for a refreshment?")
