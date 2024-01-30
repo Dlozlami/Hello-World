c129a37d94684ff055763ed87368e0b8e1faeeb5
Dlozi <dlozi.mthethwa@gmail.com>
2024-01-30 11:04:32 +0200

    feat: templates in djangoServer

--- djangoServer/views.py
+++ djangoServer/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
+#All view functions must accept a request because they are routes
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def files(request):
+    return render(request,"files/files.html",)
