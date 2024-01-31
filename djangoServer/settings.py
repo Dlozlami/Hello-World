adb935f06c082d24b70de26d21480dcea2b0348a
Dlozi <dlozi.mthethwa@gmail.com>
2024-01-31 16:12:34 +0200

    feat: render files.html page

--- djangoServer/settings.py
+++ djangoServer/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-  #  'files',
+   'djangoServer',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- djangoServer/views.py
+++ djangoServer/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 #All view functions must accept a request because they are routes
 
@@ -6,4 +7,4 @@ def index(request):
     return HttpResponse("Hello World!")
 
 def files(request):
-    return render(request,"files/files.html",)
+    return render(request,"files/files.html",{'data':'test-data'})
